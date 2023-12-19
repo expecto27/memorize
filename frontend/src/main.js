0bd85694d0064dac48c4c239bd4053c75e6d0f7c
expecto27 <expeo@vk.com>
2023-12-20 02:12:15 +0800

    auth front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue' // Импорт метода для создания приложения
 import App from './App.vue' // Импорт главного компонента
 import router from './router' // Маршрутизация
-
+import store from './store';
 const app = createApp(App); // Создание экземпляра приложения
 app.use(router); // Подключение маршрутизации
+app.use(store); 
 app.mount('#app'); // Привязка экземпляра приложения к странице HTML (находится в public)
